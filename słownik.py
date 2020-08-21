5369ac4028dfa4ead49363861c3193815e497c9c
rakiet <piter5331@gmail.com>
2020-08-21 20:25:20 +0200

    zmiana sposobu powtarzania

--- słownik.py
+++ słownik.py
@@ -4,25 +4,20 @@ dictionary = {'nabab': 'bardzo bogaty czlowiek',
               'nimfomanka': 'chorobliwy stan nadmiernego pobudzenia erotycznego u kobiet',
               'knajak': 'człowiek z marginesu spolecznego'}
 
-def start(firstTorF):
+while(True):
 
-    if firstTorF:
-        choiceInput = input("Witam w słowniku, co chcesz dzisiaj robić?\n"
+
+    choice = input("Witam w słowniku, co chcesz dzisiaj robić?\n"
                     "1. Dodaj definicje \n"
                     "2. Wyszukac definicje \n"
                     "3. Usunąc definicje\n"
                     "4. Wyświetl wszystko\n"
                     "5. Koniec\n")
-    else:
-        choiceInput = input("Witam ponownie, dokonaj wyboru.\n"
-                    "1. Dodaj definicje \n"
-                    "2. Wyszukac definicje \n"
-                    "3. Usunąc definicje\n"
-                    "4. Wyświetl wszystko\n"
-                    "5. Koniec\n")
-    choice(choiceInput)
 
-def choice(choice):
+
+
+
+
     if choice == '1':
         word = input("Podaj slowo: ")
         importance = input("Podaj definicje: ")
@@ -31,7 +26,7 @@ def choice(choice):
             print('Zapisano :)')
         else:
             print('coś poszło nie tak, może sie poprawisz :)')
-        start(False)
+        continue
 
     elif choice == '2':
         searches = input("Podaj szukane slowo: ")
@@ -42,7 +37,7 @@ def choice(choice):
             print("Definicja slowa:", searches, "to: '", importance ,"'\n\n\nKONIEC KODU")
         else:
             print("Niestety nie ma jeszcze definicji do słowa:", searches,"\nMoże zechcesz ją dodać :)")
-        start(False)
+        continue
 
     elif choice == '3':
         delWord = input("Podaj definicje która ma zostać usunięta: ")
@@ -53,20 +48,16 @@ def choice(choice):
             print("usunięto")
         else:
             print("Niestety nie można usunąć, czegoś, czego nie ma")
-        start(False)
+        continue
 
     elif choice == '4':
         for i in dictionary:
             print("Definicja slowa '", i, "' to:\n       ", dictionary[i], '\n')
-        start(False)
+        continue
 
     elif choice == '5':
         print("dziekuje, miłego dnia")
+        break
 
     else:
         print("Błąd")
-        start(False)
-
-
-
-start(True)
